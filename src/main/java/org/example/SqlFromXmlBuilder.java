056196c8472ed6e02eed3e73aa7836a9752b9d92
oogiepanda <harlan.oogie@gmail.com>
2020-07-18 16:25:53 -0700

    first commit

---
+++ src/main/java/org/example/SqlFromXmlBuilder.java
@@ -0,0 +1,157 @@
+package org.example;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Utility to SQL from input claim Xml
+ */
+public class SqlFromXmlBuilder {
+    private static int patDobMissingCounter = 0;
+    private static int mbrDobMissingCounter = 0;
+    private static int badMembrIdCounter = 0;
+    private static final int MAX_LINES = 5000;
+
+    public static final String PAT_DOB = "Patient.DateOfBirth";
+    public static final String PAT_FNAME = "Patient.IndividualName.First";
+    public static final String PAT_MI = "Patient.IndividualName.MiddleInitial";
+    public static final String PAT_LNAME = "Patient.IndividualName.Last";
+    public static final String PAT_ST = "Patient.Address.Street";
+    public static final String PAT_CITY = "Patient.Address.City";
+    public static final String PAT_STATE = "Patient.Address.State";
+    public static final String PAT_ZIP = "Patient.Address.Zip";
+
+    public static final String MBR_ID = "Insured.ID";
+    public static final String MBR_DOB = "Insured.DateOfBirth";
+    public static final String MBR_FNAME = "Insured.IndividualName.First";
+    public static final String MBR_MI = "Insured.IndividualName.MiddleInitial";
+    public static final String MBR_LNAME = "Insured.IndividualName.Last";
+    public static final String MBR_ST = "Insured.Address.Street";
+    public static final String MBR_CITY = "Insured.Address.City";
+    public static final String MBR_STATE = "Insured.Address.State";
+    public static final String MBR_ZIP = "Insured.Address.Zip";
+
+    public static final String PROV_FNAME = "HCFA1500.Practitioner.IndividualName.First";
+    public static final String PROV_LNAME = "HCFA1500.Practitioner.IndividualName.Last";
+    public static final String PROV_TAX_ID = "HCFA5100.Practitioner.FederalTaxIDNumber.Number";
+    public static final String CLM_BR_NBR = "BasicForm.BenefitRequestNumber";
+    public static final String CLM_PROC_DCN = "DCN";
+    public static final String PAT_ACCT_NBR = "HCFA5100.Patient.AccountNumber";
+
+
+    private static final String[] DEFAULT_ELEMENT_NAMES = new String[]{PAT_DOB, PAT_FNAME, PAT_MI, PAT_LNAME, PAT_ST, PAT_CITY, PAT_STATE, PAT_ZIP, MBR_FNAME, MBR_MI, MBR_LNAME, MBR_ST, MBR_CITY, MBR_STATE, MBR_ZIP, PROV_FNAME, PROV_LNAME, PROV_TAX_ID, CLM_BR_NBR, CLM_PROC_DCN, PAT_ACCT_NBR, MBR_ID, MBR_DOB};
+    private static Logger log = LoggerFactory.getLogger(SqlFromXmlBuilder.class);
+
+    private FileWriter outputFile;
+    private int fileCounter = 0;
+    private int outputFileLineCounter = 0;
+    private String inputFileName;
+
+    public SqlFromXmlBuilder(String inputFileName) {
+        try {
+            this.inputFileName = inputFileName;
+            outputFile = createFileWriter(inputFileName, MAX_LINES);
+        } catch (IOException e) {
+            log.error("Error: ", e);
+        }
+    }
+
+    private FileWriter createFileWriter(String inputFileName, int maxLines) throws IOException {
+        int lastIndex = inputFileName.lastIndexOf(File.separator);
+        String path = inputFileName.substring(0, lastIndex + 1);
+        String nuFileName = path + inputFileName.substring(lastIndex + 1, inputFileName.lastIndexOf(".")) + "_" + ++fileCounter + ".txt";
+        return new FileWriter(new File(nuFileName));
+    }
+
+    public static void main(String[] args) {
+        if (!checkArgs(args)) {
+            return;
+        }
+        SqlFromXmlBuilder sqlBuilder = new SqlFromXmlBuilder(args[0]);
+
+        try {
+            XmlScraper myScraper = new XmlScraper();
+            BufferedReader inputFile = new BufferedReader(new FileReader(args[0]));
+            String line = inputFile.readLine();
+            List<String> elementNames = sqlBuilder.createElementNames(args);
+            int lineCounter = 0;
+
+            while (line != null) {
+                List<String> values = myScraper.getValues(line, elementNames);
+                String sql = createOutputString(values, "");
+                lineCounter++;
+
+                sqlBuilder.writeSql(sql);
+                line = inputFile.readLine();
+            }
+            sqlBuilder.writeSql("commit;");
+            sqlBuilder.closeFiles();
+            System.out.println(String.format("%s PatMissingDob: %d MbrMissing: %d DobLargeMemberId: %d XmlLinesRead: %d", args[0], patDobMissingCounter, mbrDobMissingCounter, badMembrIdCounter, lineCounter));
+        } catch (Exception e) {
+            sqlBuilder.writeSql(e.getMessage());
+            log.error("Error: ", e);
+        }
+    }
+
+    private static boolean checkArgs(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please pass in a file name & search criteria, ie c:/mydir/my.xml DCN");
+            return false;
+        }
+
+        if (args.length == 1) {
+            System.out.println("Missing 2nd arg for where criteria, ie CLM_NBR, PAT_ACCT_NBR, BR, DCN ");
+            return false;
+        }
+        return true;
+    }
+
+    private static String createOutputString(List<String> values, String whereCriteria) {
+        StringBuilder sql = new StringBuilder(String.format("UPDATE PVSP001.CLM2325T SET PAT_DOB='%s',%n PAT_F_NM='%s', PAT_M_INI='%s',%n PAT_L_NM='%s',%n PAT_LN1_ADR='%s',%n PAT_CTY_ADR='%s',%n PAT_ST_ADR='%s', PAT_ZIP_ADR='%s',%n MEMBR_F_NM='%s',%n MEMBR_M_INI='%s', MEMBR_L_NM='%s',%n MEMBR_LN1_ADR='%s',%n MEMBR_CTY_ADR='%s',%n MEMBR_ST_ADR='%s', MEMBR_ZIP_ADR='%s',%n RND_PROV_F_NM='%s',%n RND_PROV_L_NM='%s',%n PRC_DPA_TAX_ID='%s'", values.toArray(new String[]{})));
+        return sql.toString();
+    }
+
+    private List<String> createElementNames(String[] inputArgs) {
+        if (inputArgs.length < 3) {
+            return Arrays.asList(DEFAULT_ELEMENT_NAMES);
+        }
+        List<String> elementNames = new ArrayList<>();
+        for (int i = 0; i < inputArgs.length; i++) {
+            if (i != 0) {
+                elementNames.add(inputArgs[i]);
+            }
+        }
+        return elementNames;
+    }
+
+    private void writeSql(String message) {
+        try {
+            if (message != null) {
+                outputFile.write(message);
+                outputFile.write(System.lineSeparator());
+                System.out.println(message);
+                if (outputFileLineCounter++ == MAX_LINES) {
+                    outputFileLineCounter = 0;
+                    outputFile.write("commit;");
+                    outputFile.close();
+                    outputFile = createFileWriter(inputFileName, MAX_LINES);
+                }
+            }
+        } catch (IOException e) {
+            log.error("Error: ", e);
+        }
+    }
+
+    private void closeFiles() {
+        try {
+            outputFile.close();
+        } catch (IOException e) {
+            log.error("Error: ", e);
+        }
+    }
+}
---
+++ src/main/java/org/example/XmlScraper.java
@@ -0,0 +1,187 @@
+package org.example;
+
+import javafx.util.Pair;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Optional;
+
+import static java.util.Arrays.stream;
+
+/**
+ * Utility to get/set xml values
+ */
+public class XmlScraper {
+
+    public static Pair createBeginEndElement(String qualifiedName) {
+        String[] names = qualifiedName.split("\\.");
+        StringBuffer key = new StringBuffer();
+        StringBuffer value = new StringBuffer();
+        for (int i = 0; i < names.length; i++) {
+            key.append("<").append(names[i]).append(">");
+            if (i != names.length - 1) { key.append(" "); }
+        }
+        value.append("</").append(names[names.length - 1]).append(">");
+        return new Pair(key.toString(), value.toString());
+    }
+
+    /**
+     * @param elementStart "<Patient> <IndividualName> <First>"
+     * @param elementEnd "</First>"
+     * @param xml
+     * @return May return an empty string but never a null
+     */
+    public String getElementValue(String xml, String elementStart, String elementEnd) {
+        String[] elements = elementStart.split(" ");
+        for (int i = 0; i < elements.length; i++) {
+            int start = xml.indexOf(elements[i]);
+            if (start > 0) { xml = xml.substring(start); }
+        }
+        int end = xml.indexOf(elementEnd);
+        if (end != -1) { return xml.substring(elements[elements.length - 1].length(), end); }
+        return "";
+    }
+
+    /**
+     * @param xml
+     * @param qualifiedNames {"ClaimLine.ServiceDate.FromDate", "ClaimLine.ServiceDate.ToDate" }
+     * @param newValue
+     * @return Newly updated xml string
+     */
+    public String setValues(String xml, List<String> qualifiedNames, String newValue) {
+        for (int i = 0; i < qualifiedNames.size(); i++) { xml = setValue(xml, qualifiedNames.get(i), newValue); }
+        return xml;
+    }
+
+    /**
+     * @param xml
+     * @param qualifiedName "HCFA1500.SpecificReserved.BenefitRequestNumber"
+     * @param newValue
+     * @return Newly updated xml string
+     */
+    public String setValue(String xml, String qualifiedName, String newValue) {
+        if (getValues(xml, qualifiedName).isEmpty()) { return xml; }
+        int parentStartIndex = 0;
+        int parentEndIndex = xml.length()-1;
+        int startIndex = 0;
+
+        for(String name : qualifiedName.split("\\.")) {
+            String beginElement = String.format("<%s", name);
+            startIndex = indexOf(xml, beginElement, parentStartIndex, parentEndIndex);
+            if (startIndex == -1 ) { return xml; }
+            parentStartIndex = startIndex;
+            parentEndIndex = xml.indexOf(String.format("</%s", name), parentStartIndex);
+        }
+        return createNewXml(xml, newValue, startIndex, parentEndIndex);
+    }
+
+    /**
+     * Method to ensure that search for non unique Xml Elements ie <DateOfBirth> are within the expected indices.
+     * @param text
+     * @param searchWord
+     * @param start
+     * @param end
+     * @return -1 for no match
+     */
+    private int indexOf(String text, String searchWord, int start, int end) {
+        int index = start;
+        while (true) {
+            index = text.indexOf(searchWord, index);
+            if (index == -1 || index < end) { return index; }
+        }
+    }
+
+    private String createNewXml(String xml, String newValue, int start, int end) {
+        String s1 = xml.substring(0, xml.indexOf('>', start) + 1);
+        String s2 = xml.substring(end);
+        StringBuffer nuXml = new StringBuffer();
+        nuXml.append(s1).append(newValue).append(s2).toString();
+        return nuXml.toString();
+    }
+
+    /**
+     * @param xml
+     * @param qualifiedName "HCFA1500.SpecificReserved.BenefitRequestNumber"
+     * @return  May return an empty string but never a null
+     */
+    public String getValue(String xml, String qualifiedName) {
+        List<String> values = getValues(xml, qualifiedName);
+        return values.isEmpty() ? "" : values.get(0);
+    }
+
+    /**
+     * @param xml
+     * @param qualifiedNames List of elements {"HCFA1500.SpecificReserved.BenefitRequestNumber", "BatchLocator"}
+     * @return  May return an empty list but never a null
+     */
+    public List<String> getValues(String xml, List<String> qualifiedNames) {
+        List<String> values = new ArrayList<>();
+        qualifiedNames.forEach(n -> {
+                                        List<String> vals = getValues(xml, n);
+                                        values.add(vals.isEmpty() ? "" : vals.get(0));
+        });
+        return values;
+    }
+
+    /**
+     * 	<BasicForm>
+     * 	<Option ProvidedBy="L" OptSvcCd="A88">QV</Option>
+     * 	<Option ProvidedBy="L">AD</Option>
+     * </BasicForm>
+     *
+     * @param xml
+     * @param qualifiedName "HCFA1500.SpecificReserved.BenefitRequestNumber"
+     * @return May return an Empty List but never a null
+     */
+    public List<String> getValues(String xml, String qualifiedName) {
+        String[] names = qualifiedName.split("\\.");
+
+        if (names.length == 0) { return Collections.EMPTY_LIST; }
+        final String[] xmls = new String[] {xml};
+
+        stream(names).forEach(n -> xmls[0] = extractXmlSubString(xmls[0], n));
+        String lastElement = names[names.length -1];
+        List<String> values = new ArrayList<>();
+
+        while(true) {
+            Optional<String> value = getValue(xmls, lastElement);
+            if (!value.isPresent())  { break; };
+            value.ifPresent(values::add);
+        }
+        return values;
+    }
+
+    private String extractXmlSubString(String xml, String name) {
+        int start = getStartIndexFromBeginOfXml(xml, name);
+        int end = getEndIndexFromBackOfXml(xml, name);
+
+        return (start == -1 || end == -1) ? xml : xml.substring(start, end);
+    }
+
+    private int getStartIndexFromBeginOfXml(String xml, String name) {
+        String element = String.format("<%s", name);
+        int index = xml.indexOf(element);
+        return (index < 0) ?  -1 : index;
+    }
+
+    private int getEndIndexFromBackOfXml(String xml, String name) {
+        String element = String.format("</%s>", name);
+        int index = xml.lastIndexOf(element);
+        return (index < 0) ?  -1 : (index  + element.length());
+    }
+
+    private Optional<String> getValue(String[] xmls, String name) {
+        if (StringUtils.isBlank(xmls[0]) || StringUtils.isBlank(name)) { return Optional.empty(); }
+        String xml = xmls[0];
+        int start = getStartIndexFromBeginOfXml(xml, name);
+        int end = xml.indexOf(String.format("</%s>", name));
+
+        if (start == -1 || end == -1) { return Optional.empty();}
+        start = xml.indexOf('>');
+
+        xmls[0] = xml.substring(end + 1 + String.format("<%s>", name).length());
+        return Optional.of(xml.substring(start + 1, end).trim());
+    }
+}
