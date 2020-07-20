d8f1b2cbc35b02cca1e94e93468f25e0f4433dcf
oogiepanda <harlan.oogie@gmail.com>
2020-07-19 19:27:48 -0700

    cleaned up the code

--- src/main/java/org/example/BRNumFileCleaner.java
+++ src/main/java/org/example/BRNumFileCleaner.java
@@ -1,60 +1,34 @@
 package org.example;
 
 import org.apache.commons.lang3.StringUtils;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.io.*;
 import java.util.*;
 import java.util.stream.Collectors;
 
 public class BRNumFileCleaner {
-    public static final String PAT_DOB = "Patient.DateOfBirth";
-    public static final String PAT_FNAME = "Patient.IndividualName.First";
-    public static final String PAT_MI = "Patient.IndividualName.MiddleInitial";
-    public static final String PAT_LNAME = "Patient.IndividualName.Last";
-    public static final String PAT_ST = "Patient.Address.Street";
-    public static final String PAT_CITY = "Patient.Address.City";
-    public static final String PAT_STATE = "Patient.Address.State";
-    public static final String PAT_ZIP = "Patient.Address.Zip";
-
-    public static final String MBR_ID = "Insured.ID";
-    public static final String MBR_DOB = "Insured.DateOfBirth";
-    public static final String MBR_FNAME = "Insured.IndividualName.First";
-    public static final String MBR_MI = "Insured.IndividualName.MiddleInitial";
-    public static final String MBR_LNAME = "Insured.IndividualName.Last";
-    public static final String MBR_ST = "Insured.Address.Street";
-    public static final String MBR_CITY = "Insured.Address.City";
-    public static final String MBR_STATE = "Insured.Address.State";
-    public static final String MBR_ZIP = "Insured.Address.Zip";
-
-    public static final String PROV_FNAME = "HCFA1500.Practitioner.IndividualName.First";
-    public static final String PROV_LNAME = "HCFA1500.Practitioner.IndividualName.Last";
-    public static final String PROV_TAX_ID = "HCFA5100.Practitioner.FederalTaxIDNumber.Number";
     public static final String CLM_BR_NBR = "BasicForm.BenefitRequestNumber";
-    public static final String CLM_PROC_DCN = "DCN";
-    public static final String PAT_ACCT_NBR = "HCFA5100.Patient.AccountNumber";
-
-
-    private static final String[] DEFAULT_ELEMENT_NAMES = new String[]{PAT_DOB, PAT_FNAME, PAT_MI, PAT_LNAME, PAT_ST, PAT_CITY, PAT_STATE, PAT_ZIP, MBR_FNAME, MBR_MI, MBR_LNAME, MBR_ST, MBR_CITY, MBR_STATE, MBR_ZIP, PROV_FNAME, PROV_LNAME, PROV_TAX_ID, CLM_BR_NBR, CLM_PROC_DCN, PAT_ACCT_NBR, MBR_ID, MBR_DOB};
-    private static Logger log = LoggerFactory.getLogger(BRNumFileCleaner.class);
 
     private FileWriter outputFileCSV;
     private FileWriter outputFileXML;
     private BufferedReader inputBufferOldVsNew;
     private BufferedReader inputBufferClaims;
     private BufferedReader inputBufferXML;
-    private int fileCounter = 0;
+    private String inputBufferOldVsNewFileName;
+    private String inputBufferClaimsFileName;
+    private String inputBufferXMLFileName;
 
     public BRNumFileCleaner(String[] fileNames) {
         try {
-            outputFileCSV = createFileWriter(fileNames[1]);
-            outputFileXML = createFileWriter(fileNames[2]);
+            outputFileCSV = createFileWriter(fileNames[1], "csv");
+            outputFileXML = createFileWriter(fileNames[2], "xml");
             inputBufferOldVsNew = new BufferedReader(new FileReader(fileNames[0]));
             inputBufferClaims = new BufferedReader(new FileReader(fileNames[1]));
             inputBufferXML = new BufferedReader(new FileReader(fileNames[2]));
+            inputBufferOldVsNewFileName = fileNames[0];
+            inputBufferClaimsFileName = fileNames[1];
+            inputBufferXMLFileName = fileNames[2];
         } catch (IOException e) {
-            log.error("Error: ", e);
+            e.printStackTrace();
         }
     }
 
@@ -70,10 +44,22 @@ public class BRNumFileCleaner {
         return inputBufferXML;
     }
 
-    private FileWriter createFileWriter(String inputFileName) throws IOException {
+    public String getInputBufferOldVsNewFileName() {
+        return inputBufferOldVsNewFileName;
+    }
+
+    public String getInputBufferClaimsFileName() {
+        return inputBufferClaimsFileName;
+    }
+
+    public String getInputBufferXMLFileName() {
+        return inputBufferXMLFileName;
+    }
+
+    private FileWriter createFileWriter(String inputFileName, String fileExtension) throws IOException {
         int lastIndex = inputFileName.lastIndexOf(File.separator);
         String path = inputFileName.substring(0, lastIndex + 1);
-        String nuFileName = path + inputFileName.substring(lastIndex + 1, inputFileName.lastIndexOf(".")) + "_output.txt";
+        String nuFileName = path + inputFileName.substring(lastIndex + 1, inputFileName.lastIndexOf(".")) + "_output." + fileExtension;
         return new FileWriter(new File(nuFileName));
     }
 
@@ -83,16 +69,16 @@ public class BRNumFileCleaner {
         }
         BRNumFileCleaner fileBuilder = new BRNumFileCleaner(args);
         try {
-            HashMap<String, List<String>> old2NuMap = buildHashMap(args, fileBuilder);
-            old2NuMap = compareAndOutputCSV(args, old2NuMap, fileBuilder);
-            compareAndOutputXML(args, old2NuMap, fileBuilder);
+            HashMap<String, List<String>> old2NuMap = buildHashMap(fileBuilder);
+            old2NuMap = compareAndOutputCSV(old2NuMap, fileBuilder);
+            compareAndOutputXML(old2NuMap, fileBuilder);
             fileBuilder.closeFiles();
         } catch (Exception e) {
             e.printStackTrace();
         }
     }
 
-    public static HashMap<String, List<String>> buildHashMap(String[] args, BRNumFileCleaner fileBuilder) {
+    public static HashMap<String, List<String>> buildHashMap(BRNumFileCleaner fileBuilder) {
         HashMap<String, List<String>> old2NuMap = new HashMap<>();
         int lineCounter = 0;
         try {
@@ -112,15 +98,14 @@ public class BRNumFileCleaner {
                 lineCounter++;
                 line = fileBuilder.getInputBufferOldVsNew().readLine();
             }
-            System.out.println(String.format("%s CsvLinesRead: %d", args[0], lineCounter));
+            System.out.println(String.format("%s CsvLinesRead: %d", fileBuilder.getInputBufferOldVsNewFileName(), lineCounter));
         } catch (Exception e) {
             e.printStackTrace();
         }
         return old2NuMap;
     }
 
-
-    public static HashMap<String, List<String>> compareAndOutputCSV(String[] args, HashMap<String, List<String>> old2NuMap, BRNumFileCleaner fileBuilder) {
+    public static HashMap<String, List<String>> compareAndOutputCSV(HashMap<String, List<String>> old2NuMap, BRNumFileCleaner fileBuilder) {
         int lineCounter = 0;
         HashMap<String, List<String>> outputLinesMap = new HashMap<>();
         try {
@@ -130,13 +115,13 @@ public class BRNumFileCleaner {
                 List<String> tokens = getTokensWithCollection(line);
                 String oldBR = tokens.get(2).trim();
                 if (old2NuMap.containsKey(oldBR)) {
-                    outputLinesMap.put(oldBR,old2NuMap.get(oldBR));
+                    outputLinesMap.put(oldBR, old2NuMap.get(oldBR));
                 }
                 lineCounter++;
                 line = reader.readLine();
             }
             fileBuilder.writeCSVText(outputLinesMap);
-            System.out.println(String.format("%s CsvLinesRead: %d", args[1], lineCounter));
+            System.out.println(String.format("%s CsvLinesRead: %d", fileBuilder.getInputBufferClaimsFileName(), lineCounter));
         } catch (Exception e) {
             System.out.println(lineCounter);
             e.printStackTrace();
@@ -144,8 +129,7 @@ public class BRNumFileCleaner {
         return outputLinesMap;
     }
 
-
-    public static void compareAndOutputXML(String[] args, HashMap<String, List<String>> old2NuMap, BRNumFileCleaner fileBuilder) {
+    public static void compareAndOutputXML(HashMap<String, List<String>> old2NuMap, BRNumFileCleaner fileBuilder) {
         int lineCounter = 0;
         try {
             XmlScraper myScraper = new XmlScraper();
@@ -159,7 +143,7 @@ public class BRNumFileCleaner {
                 lineCounter++;
                 line = reader.readLine();
             }
-            System.out.println(String.format("%s XmlLinesRead: %d", args[2], lineCounter));
+            System.out.println(String.format("%s XmlLinesRead: %d", fileBuilder.getInputBufferXMLFileName(), lineCounter));
         } catch (Exception e) {
             System.out.println(lineCounter);
             e.printStackTrace();
@@ -180,24 +164,7 @@ public class BRNumFileCleaner {
         return true;
     }
 
-    private static String createOutputString(List<String> values, String whereCriteria) {
-        return String.format("UPDATE PVSP001.CLM2325T SET PAT_DOB='%s', PAT_F_NM='%s', PAT_M_INI='%s', PAT_L_NM='%s', PAT_LN1_ADR='%s', PAT_CTY_ADR='%s', PAT_ST_ADR='%s', PAT_ZIP_ADR='%s', MEMBR_F_NM='%s', MEMBR_M_INI='%s', MEMBR_L_NM='%s', MEMBR_LN1_ADR='%s', MEMBR_CTY_ADR='%s', MEMBR_ST_ADR='%s', MEMBR_ZIP_ADR='%s', RND_PROV_F_NM='%s', RND_PROV_L_NM='%s', PRC_DPA_TAX_ID='%s'", values.toArray(new String[]{}));
-    }
-
-    private List<String> createElementNames(String[] inputArgs) {
-        if (inputArgs.length < 3) {
-            return Arrays.asList(DEFAULT_ELEMENT_NAMES);
-        }
-        List<String> elementNames = new ArrayList<>();
-        for (int i = 0; i < inputArgs.length; i++) {
-            if (i != 0) {
-                elementNames.add(inputArgs[i]);
-            }
-        }
-        return elementNames;
-    }
-
-    private void writeCSVText(HashMap<String,List<String>> outputLines) {
+    private void writeCSVText(HashMap<String, List<String>> outputLines) {
         if (outputLines != null) {
             outputLines.values().forEach(l -> writeCSVText(l));
         }
@@ -217,10 +184,9 @@ public class BRNumFileCleaner {
                 System.out.println(message);
             }
         } catch (IOException e) {
-            log.error("Error: ", e);
+            e.printStackTrace();
         }
     }
-
 
     private void writeXMLText(String message) {
         try {
@@ -230,7 +196,7 @@ public class BRNumFileCleaner {
                 System.out.println(message);
             }
         } catch (IOException e) {
-            log.error("Error: ", e);
+            e.printStackTrace();
         }
     }
 
@@ -242,7 +208,7 @@ public class BRNumFileCleaner {
             inputBufferClaims.close();
             inputBufferXML.close();
         } catch (IOException e) {
-            log.error("Error: ", e);
+            e.printStackTrace();
         }
     }
 }
